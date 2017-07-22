3f2812ab0db183612acf10e08eadc28846ab1e7d
Rafael <leafar1@gmx.net>
2017-07-22 15:01:03 +0200

    basic sprite and maze generation

--- Main.py
+++ Main.py
@@ -1,14 +1,27 @@
 import pygame
 from pygame.locals import *
 import sys
+from generation import genMaze
+from Sprite import Sprite
 
 pygame.init()
 
 screen = pygame.display.set_mode((640, 480))
 
+maze = genMaze(20)
+walls = pygame.sprite.Group()
+floors = pygame.sprite.Group()
+SCALE = 20
+for x in range(maze.M):
+    for y in range(maze.N):
+        if not maze[x,y]:
+            walls.add(Sprite("wall_placeholder.png", (x*SCALE+SCALE/2, y*SCALE+SCALE/2), SCALE))
+
 while 1:
     for event in pygame.event.get():
         if event.type in (QUIT, KEYDOWN):
             sys.exit()
+    
+    walls.draw(pygame.display.get_surface())
     pygame.display.update()
     pygame.time.delay(100)
---
+++ Sprite.py
@@ -0,0 +1,18 @@
+#/usr/bin/python
+
+import os, sys
+import pygame
+from pygame.locals import *
+
+class Sprite(pygame.sprite.Sprite):
+    
+    def __init__(self, image, position, size=None):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(
+            os.path.join('resources', 'sprites', image)).convert()
+        if size is not None:
+            if type(size) is int or type(size) is float:
+                size = size, size
+            self.image = pygame.transform.scale(self.image, size)
+        
+        self.rect = self.image.get_rect(center=position)
